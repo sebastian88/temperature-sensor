3e85325d1891186dea2780f354610791ea9e3427
rogers101 <sebastian.rogers@bbc.co.uk>
2023-08-10 09:23:07 +0100

    fix main.js once and for all.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from machine import Pin
-from machine import ADC
-from dht import DHT11, InvalidChecksum
+from dht import DHT11
 import time, ntptime
 import network
 import urequests as requests
 import wifi_secrets
 
-try:
 
+def flash(led, times):
+    while times > 0:
+        led.on()
+        time.sleep(0.2)
+        led.off()
+        time.sleep(0.2)
+        times = times - 1
+
+def date(st):
+    return"{:4}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}Z".format( \
+        st[0], st[1], st[2], st[3], st[4], st[5])
+
+def setup():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(wifi_secrets.ssid, wifi_secrets.password)
@@ -16,19 +27,8 @@ try:
 
     pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
     sensor = DHT11(pin)
-    led = machine.Pin("LED", machine.Pin.OUT)
+    led = Pin("LED", Pin.OUT)
 
-    def date(st):
-        return"{:4}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}Z".format( \
-            st[0], st[1], st[2], st[3], st[4], st[5])
-
-    def flash(times):
-        while times > 0:
-            led.on()
-            time.sleep(0.2)
-            led.off()
-            time.sleep(0.2)
-            times = times - 1
 
     # Wait for connect or fail
     max_wait = 10
@@ -45,7 +45,6 @@ try:
     print('connected')
     status = wlan.ifconfig()
     print( 'ip = ' + status[0] )
-    request_headers = {'Content-Type': 'application/json'}
     
     time_got = False
     while time_got is False:
@@ -54,34 +53,43 @@ try:
             time_got = True
         except Exception as e:
             print(e)
+    return sensor, led
 
-    flash(wifi_secrets.room)
-    time.sleep(1.0)
-    flash(wifi_secrets.room)
-    time.sleep(1.0)
-    flash(wifi_secrets.room)
+def readTemperature(sensor):
+    current_time = date(time.localtime())
     
+    temperature_got = False
+    temperature = None
+    humidity = None
+    while temperature_got is False:
+        try:
+            temperature = sensor.temperature
+            humidity = sensor.humidity
+            temperature_got = True
+        except Exception as e:
+            print(e)
+    
+    sensor_readings={'room': wifi_secrets.room,'time': current_time, 'temperature': temperature, 'humidity': humidity}
+    request = requests.post(wifi_secrets.url,json=sensor_readings,headers={'Content-Type': 'application/json'})
+    request.close()
 
-    while True:
-        current_time = date(time.localtime())
-        
-        temperature_got = False
-        temperature = None
-        humidity = None
-        while temperature_got is False:
-            try:
-                temperature = sensor.temperature
-                humidity = sensor.humidity
-                temperature_got = True
-            except Exception as e:
-                print(e)
-        
-        sensor_readings={'room': wifi_secrets.room,'time': current_time, 'temperature': temperature, 'humidity': humidity}
-        request = requests.post(wifi_secrets.url,json=sensor_readings,headers=request_headers)
-        request.close()
-        time.sleep(300)
-            
-except Exception as e:
-    print(e)
 
-time.sleep(60)
+while True:
+    try:
+        sensor, led = setup()
+        
+        flash(led, wifi_secrets.room)
+        time.sleep(1.0)
+        flash(led, wifi_secrets.room)
+        time.sleep(1.0)
+        flash(led, wifi_secrets.room)
+
+
+        while True:
+            readTemperature(sensor)
+            time.sleep(300)
+
+    except Exception as e:
+        print(e)
+
+    time.sleep(60)
